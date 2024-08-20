5078bc71499f9816a599ca15cdb5f41028d14207
Shahul_t_a <94727254+shahultas@users.noreply.github.com>
2024-08-20 13:19:24 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -4,7 +4,12 @@ import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-
+  apiKey: "AIzaSyBzeYuqF8PRc01rJ2TjNaMrzMtoB8DnVqM",
+  authDomain: "file-upload-12bb9.firebaseapp.com",
+  projectId: "file-upload-12bb9",
+  storageBucket: "file-upload-12bb9.appspot.com",
+  messagingSenderId: "64640652437",
+  appId: "1:64640652437:web:60865be911f7d064f4b79c",
 };
 
 // Initialize Firebase
